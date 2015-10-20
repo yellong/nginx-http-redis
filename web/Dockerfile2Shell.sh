7d4f26939e3e15e0c19967938bda99ebd761f1e2
yellong <yellongweek@gmail.com>
2015-10-20 11:45:49 +0800

    update

---
+++ web/Dockerfile2Shell.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+cd ~
+
+# Download Nginx and Nginx modules source
+wget http://nginx.org/download/nginx-1.8.0.tar.gz -O nginx.tar.gz 
+mkdir /tmp/nginx 
+tar -xzvf nginx.tar.gz -C /tmp/nginx --strip-components=1
+
+wget http://people.freebsd.org/~osa/ngx_http_redis-0.3.7.tar.gz -O ngx-http-redis.tar.gz 
+mkdir /tmp/nginx/http-redis 
+tar -xzvf ngx-http-redis.tar.gz -C /tmp/nginx/http-redis --strip-components=1
+
+wget https://github.com/maxmind/libmaxminddb/releases/download/1.1.1/libmaxminddb-1.1.1.tar.gz -O libmaxminddb.tar.gz 
+mkdir /tmp/nginx/libmaxminddb 
+tar -xzvf libmaxminddb.tar.gz -C /tmp/nginx/libmaxminddb --strip-components=1
+cd /tmp/nginx/libmaxminddb 
+./configure 
+make 
+make check 
+make install 
+ldconfig
+
+# https://github.com/leev/ngx_http_geoip2_module
+wget https://github.com/leev/ngx_http_geoip2_module/archive/1.0.tar.gz -O ngx-http-geoip2.tar.gz 
+mkdir /tmp/nginx/http-geoip2 
+tar -xzvf ngx-http-geoip2.tar.gz -C /tmp/nginx/http-geoip2 --strip-components=1
+
+# https://hub.docker.com/r/unknownhero/ubuntu-nginx-lua/~/dockerfile/
+wget http://luajit.org/download/LuaJIT-2.0.3.tar.gz -O LuaJIT-2.0.3.tar.gz 
+mkdir /tmp/LuaJIT-2.0.3 
+tar -xzvf LuaJIT-2.0.3.tar.gz -C /tmp/LuaJIT-2.0.3 --strip-components=1 
+cd /tmp/LuaJIT-2.0.3 
+make 
+make install
+
+wget http://labs.frickle.com/files/ngx_cache_purge-2.3.tar.gz -O ngx_cache_purge-2.3.tar.gz 
+mkdir /tmp/nginx/ngx_cache_purge 
+tar -xzvf ngx_cache_purge-2.3.tar.gz -C /tmp/nginx/ngx_cache_purge --strip-components=1
+
+wget https://github.com/openresty/lua-nginx-module/archive/v0.9.13.zip -O lua-nginx-module-0.9.13.zip 
+unzip lua-nginx-module-0.9.13.zip -d /tmp/nginx 
+mv /tmp/nginx/lua-nginx-module-0.9.13 /tmp/nginx/lua-nginx-module
+
+wget https://github.com/simpl/ngx_devel_kit/archive/v0.2.19.zip -O ngx_devel_kit-0.2.19.zip 
+unzip ngx_devel_kit-0.2.19.zip -d /tmp/nginx 
+mv /tmp/nginx/ngx_devel_kit-0.2.19 /tmp/nginx/ngx_devel_kit
+
+cd /tmp/nginx
+
+export LUAJIT_LIB=/usr/local/lib 
+export LUAJIT_INC=/usr/local/include/luajit-2.0 
+ln -s /usr/local/lib/libluajit-5.1.so.2 /lib64/libluajit-5.1.so.2 
+
+./configure --sbin-path=/usr/local/sbin \
+--with-ld-opt=-Wl,-rpath,/usr/local/lib \
+--prefix=/usr/share/nginx \
+--conf-path=/etc/nginx/nginx.conf \
+--pid-path=/run/nginx.pid \
+--lock-path=/run/lock/subsys/nginx \
+--error-log-path=/var/log/nginx/error.log \
+--http-log-path=/var/log/nginx/access.log \
+--with-http_gzip_static_module \
+--with-http_stub_status_module \
+--with-http_ssl_module \
+--with-http_spdy_module \
+--with-pcre \
+--with-http_image_filter_module \
+--with-file-aio \
+--with-ipv6 \
+--with-http_dav_module \
+--with-http_flv_module \
+--with-http_mp4_module \
+--with-http_gunzip_module \
+--with-http_realip_module \
+--with-http_auth_request_module \
+--with-http_addition_module \
+--with-http_sub_module \
+--add-module=http-redis \
+--add-module=http-geoip2 \
+--add-module=./lua-nginx-module \
+--add-module=./ngx_devel_kit \
+--add-module=./ngx_cache_purge 
+make -j2 
+make install
--- web/files/languages-countries.lua
+++ web/files/languages-countries.lua
@@ -1,64 +1,68 @@
 country_langs = {
-  us='en',    -- United States, North America
-  ca='en',    -- Canada, North America
-  pr='en',    -- puerto rico, North America
+  us='',    -- United States, North America
+  ca='',    -- Canada, North America
+  pr='',    -- puerto rico, North America
 
-  co='en',   -- Colombia, South America
+  co='',   -- Colombia, South America
 
-  be='en',    -- Belgium, enope
-  dk='en',    -- Denmark, enope
+  be='',    -- Belgium, enope
+  dk='',    -- Denmark, enope
   fr='fr',    -- France, enope
   de='de',    -- Germany, enope
-  gr='en',    -- Greece, enope
-  ie='en',    -- Ireland, enope
-  it='en',    -- Italy, enope
+  gr='',    -- Greece, enope
+  ie='',    -- Ireland, enope
+  it='',    -- Italy, enope
   lu='fr',    -- Luxembourg, enope
-  nl='en',    -- Netherlands, enope
-  pt='en',    -- Portugal, enope
-  es='en',    -- Spain, enope
-  gb='en',    -- United Kingdom, enope
+  nl='',    -- Netherlands, enope
+  pt='',    -- Portugal, enope
+  es='',    -- Spain, enope
+  gb='',    -- United Kingdom, enope
   at='de',    -- Austria, enope
-  cy='en',    -- Cyprus, Asia
-  cz='en',    -- Czech Republic, enope
-  ee='en',    -- Estonia, enope
-  fi='en',    -- Finland, enope
-  lv='en',    -- Latvia, enope
+  cy='',    -- Cyprus, Asia
+  cz='',    -- Czech Republic, enope
+  ee='',    -- Estonia, enope
+  fi='',    -- Finland, enope
+  lv='',    -- Latvia, enope
   li='de',    -- Liechtenstein, enope
-  mt='en',    -- Malta, enope
+  mt='',    -- Malta, enope
   mc='fr',    -- Monaco, enope
-  no='en',    -- Norway, enope
-  pl='en',    -- Poland, enope
-  si='en',    -- Slovenia, enope
-  se='en',    -- Sweden, enope
-  ch='en',    -- Switzerland, enope
+  no='',    -- Norway, enope
+  pl='',    -- Poland, enope
+  si='',    -- Slovenia, enope
+  se='',    -- Sweden, enope
+  ch='',    -- Switzerland, enope
   hk='zh-tw',    -- Hong Kong, Asia
   mo='zh-tw',    -- Macao, Asia
   tw='zh-tw',    -- Taiwan, Republic Of China, Asia
   jp='ja',    -- Japan, Asia
-  au='en',    -- Australia, Australia
-  nz='en',    -- New Zealand, Australia
+  au='',    -- Australia, Australia
+  nz='',    -- New Zealand, Australia
   kr='kr',    -- Korea, Republic of, Asia
-  sg='en',    -- Singapore, Asia
-  bg='en',    -- bulgaria, Europe
-  hr='en',    -- croatia, Europe
-  hu='en',    -- hungary, Europe
-  lt='en',    -- lithuania, Europe
-  ro='en',    -- romania, Europe
-  sk='en',    -- slovakia, Europe
+  sg='',    -- Singapore, Asia
+  bg='',    -- bulgaria, Europe
+  hr='',    -- croatia, Europe
+  hu='',    -- hungary, Europe
+  lt='',    -- lithuania, Europe
+  ro='',    -- romania, Europe
+  sk='',    -- slovakia, Europe
   cn='cn',  -- China, Asia
-  my='en'   -- malaysia, Asia
+  my=''   -- malaysia, Asia
 }
 
 function get_lang(country)
   return country_langs[string.lower(country)]
 end
 
-
 real_locale = get_lang(ngx.var.country)
-if real_locale == 'en' then
-  real_locale = ""
-end
 if real_locale ~= ngx.var.locale then
-  return ngx.redirect("/"..real_locale..ngx.var.path_cut)
-  -- st = "your country is "..ngx.var.country..", your language should be "..real_locale..", url locale is "..ngx.var.locale..", path cut is "..ngx.var.path_cut
+  if real_locale == "" then
+    if ngx.var.path_cut == "" then
+      ngx.var.rurl = "/"
+    else
+      ngx.var.rurl = ngx.var.path_cut
+    end
+  else
+    ngx.var.rurl = "/"..real_locale..ngx.var.path_cut
+  end
+  return "redirect"
 end
